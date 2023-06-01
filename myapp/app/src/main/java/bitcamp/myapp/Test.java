98c80899eea3f207a8e7f383faa30f570f61a767
wjswnsgh98 <jeonjunho777@naver.com>
2023-06-01 15:36:10 +0900

    6/1 학습내용

--- myapp/app/src/main/java/bitcamp/myapp/Test.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test.java
@@ -1,7 +1,8 @@
 package bitcamp.myapp;
 
 public class Test {
-  public static void main(String[] args){
-    int[] no = new int[5];    
+  public static void main(String[] args) { 
+    int[] no = {100, 200, 300};
+    System.out.println(no.length);   
   }
 }
