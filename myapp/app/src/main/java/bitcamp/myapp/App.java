4baebc244427a66c7af9610d3a303c5f94c27de9
wjswnsgh98 <jeonjunho777@naver.com>
2023-06-02 18:20:05 +0900

    6/2 학습내용

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,88 +1,89 @@
 package bitcamp.myapp;
 
-// 코드 본문에서 사용할 클래스가 어떤 패키지의 클래스인지 저장한다.
 import java.util.Scanner;
 
 public class App {
-  public static void main(String[] args) {
-    // 키보드 스캐너 준비
-    Scanner scanner = new Scanner(System.in);
-    final int MAX_SIZE = 100;
-    int userId = 1;
-    int length = 0;
+  
+  static Scanner scanner = new Scanner(System.in);
 
-    int[] no = new int[MAX_SIZE];
-    String[] name = new String[MAX_SIZE];
-    String[] email = new String[MAX_SIZE];
-    String[] password = new String[MAX_SIZE];
-    char[] gender = new char[MAX_SIZE];
+  static final int MAX_SIZE = 100;
+  static int[] no = new int[MAX_SIZE];
+  static String[] name = new String[MAX_SIZE];
+  static String[] email = new String[MAX_SIZE];
+  static String[] password = new String[MAX_SIZE];
+  static char[] gender = new char[MAX_SIZE];
+  static int userId = 1;
+  static int length = 0;
+  
+  public static void main(String[] args) {
 
     printTitle();
 
-    // 회원정보 등록
-    for (int i = 0; i < MAX_SIZE; i++) {
-      inputMember(scanner, i, name, email, password, gender, no, userId++);
-      length++;
-      if(!promptContinue(scanner)){
+    while (length < MAX_SIZE) {
+      inputMember();
+      if (!promptContinue()) {
         break;
       }
     }
-    printMembers(length, no, name, email, gender);
+
+    printMembers();
+
+    scanner.close();
   }
-    
-  static void printTitle(){
+
+  static void printTitle() {
     System.out.println("나의 목록 관리 시스템");
-    System.out.println("-------------------------------");
+    System.out.println("----------------------------------");
   }
-  
-  static void inputMember(Scanner scanner, int i, String[] name, 
-  String[] email, String[] password, char[] gender, int[] no, int userId){
-    
-      System.out.print("이름? ");
-      name[i] = scanner.next();
 
-      System.out.print("이메일? ");
-      email[i] = scanner.next();
+  static void inputMember() {
 
-      System.out.print("암호? ");
-      password[i] = scanner.next();
+    System.out.print("이름? ");
+    name[length] = scanner.next();
 
-      loop: while (true){
-        System.out.println("성별: ");
-        System.out.println("  1. 남자");
-        System.out.println("  2. 여자");
-        System.out.print("> ");
-        String menuNo = scanner.next();
-        scanner.nextLine(); // 입력 값(token)을 읽고 난 후에 남아 있는 줄바꿈 코드를 제거한다.
+    System.out.print("이메일? ");
+    email[length] = scanner.next();
 
-          switch (menuNo){
-            case "1":
-              gender[i] = 'M';
-              break loop;
-            case "2":
-              gender[i] = 'W';
-              break loop;
-            default:
-              System.out.println("무효한 번호입니다.");
-          }
+    System.out.print("암호? ");
+    password[length] = scanner.next();
+
+    loop: while (true) {
+      System.out.println("성별: ");
+      System.out.println("  1. 남자");
+      System.out.println("  2. 여자");
+      System.out.print("> ");
+      String menuNo = scanner.next();
+      scanner.nextLine(); // 입력 값(token)을 읽고 난 후에 남아 있는 줄바꿈 코드를 제거한다.
+
+      switch (menuNo) {
+        case "1":
+          gender[length] = 'M';
+          break loop;
+        case "2":
+          gender[length] = 'W';
+          break loop;
+        default:
+          System.out.println("무효한 번호입니다.");
       }
-      no[i] = userId++;
+    }
+
+    no[length] = userId++;
+    length++;
   }
 
-  static boolean promptContinue(Scanner scanner){
-    System.out.print("계속 하시겠습니까?(Y/N) ");
+  static boolean promptContinue() {
+    System.out.print("계속 하시겠습니까?(Y/n) ");
     String response = scanner.nextLine();
-    if(!response.equals("") && !response.equalsIgnoreCase("Y")){
+    if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
       return false;
     }
     return true;
-    // equalsIgnoreCase : 대소문자 구분 안함.
   }
 
-  static void printMembers(int length, int[] no, String[] name, String[] email, char[] gender){
-    System.out.println("------------------------------");  
+  static void printMembers() {
+    System.out.println("---------------------------------------");
     System.out.println("번호, 이름, 이메일, 성별");
-    System.out.println("------------------------------");
+    System.out.println("---------------------------------------");
 
     for (int i = 0; i < length; i++) {
       System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
--- myapp/app/src/main/java/bitcamp/myapp/Test.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test.java
@@ -1,8 +1,16 @@
 package bitcamp.myapp;
 
 public class Test {
-  public static void main(String[] args) { 
-    int[] no = {100, 200, 300};
-    System.out.println(no.length);   
+  public static void main(String[] args) {
+    System.out.println(args); 
+    int a = 100, b = 200;
+    swap(a, b);
+    System.out.printf("main(): %d, %d\n", a, b);
+  }
+  static void swap(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.printf("swap(): %d, %d\n", a, b);
   }
 }
