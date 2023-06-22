a24bac1e0a341c99288409c86a1aba17a505e5cd
junho <jeonjunho@jeonjunhos-MacBook-Pro.local>
2023-06-22 17:43:39 +0900

    6/22 학습내용

---
+++ myapp/app/src/main/java/bitcamp/test/Exam01.java
@@ -0,0 +1,28 @@
+package bitcamp.test;
+
+class A{
+  int v1 = 100;
+  int v2 = 200;
+  {
+    System.out.println("111");
+    System.out.println("222");
+    System.out.println("333");
+  }
+  public A() {
+    System.out.println("444");
+  }
+  public A(int a) {
+    System.out.println("555");
+  }
+  public A(int a, int b) {
+    System.out.println("666");
+  }
+}
+
+public class Exam01 {
+  public static void main(String[] args) {
+    new A();
+    new A();
+    new A();
+  }
+}
