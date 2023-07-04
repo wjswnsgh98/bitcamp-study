bd20a19c494abb932a022c0a1601d74308879560
junho <jeonjunho@jeonjunhos-MacBook-Pro.local>
2023-07-04 15:54:32 +0900

    test

---
+++ m_project/app-36/src/main/java/m_project/handler/MemberAddListener.java
@@ -0,0 +1,25 @@
+package m_project.handler;
+
+import java.util.List;
+import m_project.vo.Member;
+import util.BreadcrumbPrompt;
+
+public class MemberAddListener extends AbstractMemberListener{
+
+  public MemberAddListener(List<Member> list){
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt){
+    Member m = new Member();
+    m.setBook_no(Member.userId++);
+    m.setB_title(prompt.inputString("도서제목? "));
+    m.setAuthor(prompt.inputString("글쓴이? "));
+    m.setName(prompt.inputString("이름? "));
+    m.setP_num(prompt.inputString("핸드폰번호? "));
+    m.setGender(inputGender((char)0, prompt));
+
+    this.list.add(m);
+  }
+}
